10d19f9fe74c2e068038918b12a6ebd74081da9a
Arira-re <reukiowequ723@gmail.com>
2025-07-03 12:02:21 +0900

    2.5 p112まで  - 新規pipあり

--- apps/app.py
+++ apps/app.py
@@ -2,9 +2,10 @@ from pathlib import Path
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import CSRFProtect
 # SQLAlchemyをインスタンス化する
 db = SQLAlchemy()
-
+csrf = CSRFProtect()
 def create_app():
     # Flaskインスタンス作成
     app = Flask(__name__)
@@ -12,7 +13,11 @@ def create_app():
         SECRET_KEY='abcdef',
         SQLALCHEMY_DATABASE_URI=f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
         SQLALCHEMY_TRACK_MODIFICATIONS=False,
+        # SQLをコンソールログに出力する設定
+        SQLALCHEMY_ECHO=True,
+        WTF_CSRF_SECRET_KEY='abcdef'
     )
+    csrf.init_app(app)
     # SQLAlchemyをアプリにバインドするを連携する
     db.init_app(app)
     # Migrateとアプリを連携する
