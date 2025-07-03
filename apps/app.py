ec60d7115d9d5bab8d17b1d08f5909199ee99a91
Arira-re <reukiowequ723@gmail.com>
2025-07-03 11:19:17 +0900

    マイグレーション前コミット

--- apps/app.py
+++ apps/app.py
@@ -1,8 +1,24 @@
+from pathlib import Path
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+# SQLAlchemyをインスタンス化する
+db = SQLAlchemy()
 
 def create_app():
-    app= Flask(__name__)
-
+    # Flaskインスタンス作成
+    app = Flask(__name__)
+    app.config.from_mapping(
+        SECRET_KEY='abcdef',
+        SQLALCHEMY_DATABASE_URI=f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+    )
+    # SQLAlchemyをアプリにバインドするを連携する
+    db.init_app(app)
+    # Migrateとアプリを連携する
+    migrate = Migrate(app, db)
+    # crudパッケージからviewsをインポートする
     from apps.crud import views as crud_views
+    # register_blueprintを使いcrudをアプリへ登録する
     app.register_blueprint(crud_views.crud, url_prefix='/crud')
     return app
--- apps/crud/views.py
+++ apps/crud/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-
+# Blueprintオブジェクトを生成する
 crud = Blueprint(
     'crud',
     __name__,
@@ -7,6 +7,8 @@ crud = Blueprint(
     static_folder='static',
     url_prefix='/crud'
 )
+
+# indexエンドポイントを作成しindex.htmlを返す
 @crud.route('/')
 def index():
     return render_template('crud/index.html')
